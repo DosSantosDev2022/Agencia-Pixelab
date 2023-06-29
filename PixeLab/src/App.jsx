7a566eecab375d2113d3d15104047c401895b420
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-06-29 14:31:10 -0300

    projeto iniciado

---
+++ PixeLab/src/App.jsx
@@ -0,0 +1,16 @@
+import './scss/main.scss'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+        <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ PixeLab/src/components/Header.jsx
@@ -0,0 +1,26 @@
+
+function Header(){
+  return(
+    < >
+          <footer className="footer__container">
+        <div className="navBrand">
+          <img className="logo" src="src/img/Logo.svg" alt="" />
+        </div>
+        <nav className="navBar">
+          <ul className="navBar__list">
+            <li className="navBar__links"><a className="link" href="/">Home</a></li>
+            <li className="navBar__links"><a className="link" href="/">About Us</a></li>
+            <li className="navBar__links"><a className="link" href="/">Our app</a></li>
+            <li className="navBar__links"><a className="link" href="/">Contact</a></li>
+          </ul>
+        </nav>
+        <div className="navBar__btn">
+          <a className="link" href="/">Log in</a>
+          <a className="link" href="/">Sign up</a>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Header
