6e4e2e2cfb8e535bcb5ae5c2fe7f469bfdcc7456
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-06-30 22:24:54 -0300

    seção 02 criada

--- PixeLab/src/components/Main.jsx
+++ PixeLab/src/components/Main.jsx
@@ -12,6 +12,32 @@ function Main(){
           <img src="/src/img/img.png" alt="" />
         </div>
       </section>
+      <section className="section__02">
+        <div className="row__01">
+          <p>SERVICE</p>
+          <h2>Our Vison & Our Goal</h2>
+        </div>
+        <div className="row__02">
+          <div className="card">
+            <img src="/src/img/vector.svg" alt="" />
+            <h3>Graphic Design</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dui mi, bibendum eu erat id, ultricies semper metus. Nunc dapibus laoreet dolor nec imperdiet.</p>
+            <a href="/">Learn More</a>
+          </div>
+          <div className="card">
+            <img src="/src/img/video.svg" alt="" />
+            <h3>Video Editing</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dui mi, bibendum eu erat id, ultricies semper metus. Nunc dapibus laoreet dolor nec imperdiet.</p>
+            <a href="/">Learn More</a>
+          </div>
+          <div className="card">
+            <img src="/src/img/Chart.svg" alt="" />
+            <h3>Digital Marketing</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dui mi, bibendum eu erat id, ultricies semper metus. Nunc dapibus laoreet dolor nec imperdiet.</p>
+            <a href="/">Learn More</a>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
