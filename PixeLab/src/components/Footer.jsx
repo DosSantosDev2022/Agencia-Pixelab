b7dc0a6057343a81ebfaee31feaca7b1bbe555b1
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-07-10 21:46:54 -0300

    Construindo footer da página

--- PixeLab/src/components/Footer.jsx
+++ PixeLab/src/components/Footer.jsx
@@ -2,6 +2,21 @@
 function Footer(){
   return (
     <>
+    <div className="footer">
+      <div className="row__01">
+        <img src="/src/img/Logo.svg" alt="" />
+        <p>© 2023 Epixelab. All rights reserved.</p>
+        <ul className="footer__list">
+          <li className="footer__links"><a href="/">Portfolio</a></li>
+          <li className="footer__links"><a href="/">How it Works</a></li>
+          <li className="footer__links"><a href="/">Pricing</a></li>
+          <li className="footer__links"><a href="/">About</a></li>
+        </ul>
+      </div>
+      <div className="row__02">
+        <p>Startup Framework contains components and complex blocks which can easily be integrated into almost any design. </p>
+      </div>
+    </div>
     
     </>
   )
